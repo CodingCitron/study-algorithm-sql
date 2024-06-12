cc71acb1fb27accd1b3a5bb00d93a087f70ba9b5
CodingCitron <78482307+CodingCitron@users.noreply.github.com>
2024-06-13 01:14:54 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,16 +1,10 @@
 function solution(s) {        
-    var answer = '';
-   let result = s.split(' ');
-
-   let result2 = result.map(x=> x.charAt(0).toUpperCase() + x.slice(1).toLowerCase());
-
-   answer = result2.join(" ")
-    return answer;
+    return change(s).join(" ");
 }
 
 function change(spells) {    
-    return spells.split(' ').filter(spell => spell !== '').map(spell => {
-        const first = spell[0].toUpperCase()
+    return spells.split(' ').map(spell => {
+        const first = spell.charAt(0).toUpperCase()
         return first + spell.slice(1).toLowerCase()
     })
 }
